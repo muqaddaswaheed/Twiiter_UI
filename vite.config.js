ca957bdd886e53fc653c3e6166b9f00f8747e111
muqaddas <muqaddaswaheed0@gmail.com>
2025-02-04 11:04:50 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  build: {
-    outDir: "dist", // Ensures Vercel picks the correct folder
-  }, server: {
-    historyApiFallback: true // Fixes 404 errors on refresh
-  }
 })
