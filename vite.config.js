76c5cd6a8e219af2206a3d093e650a5d46ad41e5
muqaddas <muqaddaswaheed0@gmail.com>
2025-02-04 10:33:09 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000, // Optional: Change the local dev port
-  },
+  plugins: [
+    tailwindcss(),
+  ],
   build: {
     outDir: "dist", // Ensures Vercel picks the correct folder
   }
-});
+})
